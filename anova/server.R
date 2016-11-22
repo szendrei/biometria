2f7733a287806bae8405a7af45bacbb82c9b78f2
Dora Szendrei <szendrgigi@gmail.com>
2016-11-22 16:41:06 +0100

    Working ANOVA app without Kruskal-Wallis test (just for normal distribution)

--- anova/server.R
+++ anova/server.R
@@ -1,1 +1,59 @@
 library(shiny)
+library(ggplot2)
+
+shinyServer(function(input,output){
+  
+  #helpers for plot
+  min.mean.sd.max <- function(x) {
+    r <- c(min(x),mean(x) - sd(x),mean(x),mean(x) + sd(x),max(x))
+    names(r) <- c("ymin","lower","middle","upper","ymax")
+    r
+  }
+  
+  #reactives
+  numSliders <- reactive({
+    as.integer(input$sampNum)
+  })
+  data <- reactive({
+    value <- lapply(1:numSliders(), function(i) {
+      req(mean <- as.integer(input[[paste(i)]]))
+      c(rnorm(input$length, mean, input$sd))
+    })
+    group <- factor(rep(1:numSliders(), each=input$length))
+    data <- data.frame("value"=unlist(value), "group"=group)
+  })
+  
+  #for the inputs (means)
+  output$sliders <- renderUI({
+    lapply(1:numSliders(), function(i) {
+      sliderInput(paste(i),label=paste(i,". minta átlaga:"),min=50,max=150,value=100)
+    })
+  })
+  
+  #outputs
+  output$plot <- renderPlot({
+    p1 <- ggplot(aes(y = value, x = factor(group)),data=data())
+    p1 <- p1 + stat_summary(fun.data=min.mean.sd.max,geom="boxplot") + 
+      geom_point(position="identity") + 
+      ggtitle("Minták eloszlása, átlaga és szórása") +
+      xlab("csoportok") + ylab("értékek")
+    print(p1)
+  })
+  
+  output$stats <- renderPrint({
+    print(summary(m <- aov(value~group, data=data())))
+    if (input$sampNum != 2 && summary(m)[[1]][["Pr(>F)"]][1] < .05) {
+      cat('\n')
+      print(TukeyHSD(m))
+    }
+    if (input$sampNum == 2) {
+      cat("", "Két minta esetén ellenőrizheted, hogy a t-próba ugyanazt az eredmény adja:",sep='\n')
+      t.test(value~group, data=data(), var.equal=T)
+      
+    }
+  })
+  
+  output$stats2 <- renderPrint({
+    
+  })
+})
--- anova/ui.R
+++ anova/ui.R
@@ -1,1 +1,17 @@
 library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("ANOVA"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("sampNum",label="Minták száma:",min=2,max=10,value=4),
+      sliderInput("length",label="Minták elemszáma:",min=3,max=50,value=30),
+      sliderInput("sd",label="Minták szórása:",min=1,max=50,value=10),
+      uiOutput("sliders")
+    ),
+    mainPanel(
+      plotOutput("plot"),
+      verbatimTextOutput("stats")
+    )
+  )
+))
